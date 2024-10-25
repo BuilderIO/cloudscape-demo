c4d1001a19becba5e507ead29131fc25c74bd2a7
Steve Sewell <steve@builder.io>
2024-10-25 08:57:42 -0700

    disable sever rendering for now

--- app/figma-imports/page.tsx
+++ app/figma-imports/page.tsx
@@ -13,21 +13,10 @@ interface PageProps {
 export default async function Page(props: PageProps) {
   const builderModelName = "figma-imports";
 
-  const content = await builder
-    // Get the page content from Builder with the specified options
-    .get(builderModelName, {
-      userAttributes: {
-        // Use the page path specified in the URL to fetch the content
-        urlPath: "/" + (props?.params?.page?.join("/") || ""),
-      },
-    })
-    // Convert the result to a promise
-    .toPromise();
-
   return (
     <>
       {/* Render the Builder page */}
-      <RenderBuilderContent content={content} model={builderModelName} />
+      <RenderBuilderContent model={builderModelName} />
     </>
   );
 }
