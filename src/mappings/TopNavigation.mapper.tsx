edeecf6499b45ac4e049f08b5a7e316c413c11cf
Steve Sewell <steve@builder.io>
2024-10-29 11:10:56 -0700

    fixup

--- src/mappings/TopNavigation.mapper.tsx
+++ src/mappings/TopNavigation.mapper.tsx
@@ -60,27 +60,6 @@ figmaMapping({
               { id: "profile", text: "Profile" },
               { id: "preferences", text: "Preferences" },
               { id: "security", text: "Security" },
-              {
-                id: "support-group",
-                text: "Support",
-                items: [
-                  {
-                    id: "documentation",
-                    text: "Documentation",
-                    href: "#",
-                    external: true,
-                    externalIconAriaLabel: " (opens in new tab)",
-                  },
-                  { id: "support", text: "Support" },
-                  {
-                    id: "feedback",
-                    text: "Feedback",
-                    href: "#",
-                    external: true,
-                    externalIconAriaLabel: " (opens in new tab)",
-                  },
-                ],
-              },
               { id: "signout", text: "Sign out" },
             ],
           },
